82684db017565546ba78e1e77d568a990a137a79
craig-acutest <craig.williams@acutesales.co.uk>
2014-01-27 12:14:40 +0000

    Add admin area

---
+++ Rendezvous/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Rendezvous.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "Admin_default",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { AreaName= "Admin", controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Rendezvous.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ Rendezvous/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rendezvous.Areas.Admin.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //
+        // GET: /Admin/Service/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
