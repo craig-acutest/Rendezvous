c3955c28333973d3704dadd4db0f0f4144f44dce
craig-acutest <craig.williams@acutesales.co.uk>
2014-01-27 11:36:57 +0000

    Add Chat with signalr

--- Rendezvous/Controllers/HomeController.cs
+++ Rendezvous/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace Rendezvous.Controllers
 
             return View();
         }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
     }
 }
--- Rendezvous/Startup.cs
+++ Rendezvous/Startup.cs
@@ -9,6 +9,7 @@ namespace Rendezvous
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
